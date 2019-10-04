9c590b7cc7042a9e7551a28a57eb15d98b272912
Adebayo <adebayooriyomi@gmail.com>
2019-10-03 23:01:36 -0500

    Initial Commit

---
+++ DailyNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyNews
+//
+//  Created by Adebayo Oriyomi on 2019-10-03.
+//  Copyright © 2019 Adebayo Oriyomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
